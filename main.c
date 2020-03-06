20fe125dd15375cf03774211df433d3436271292
OJI Alec-Chima <Alec-Chima.OJI@cpnv.ch>
2020-03-06 15:05:20 +0100

    wip

--- main.c
+++ main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+
+   int déjajoué;
+   int jamaisjoué;
+   int adressemail;
+   int age;
+   int motdepasse;
+   int pseudo;
+   int prénom;
+
+
+
+    printf("Bataille Navale");
+
+
+    if (jamaisjoué)
+
+    {
+
+        printf("Entrez votre adresse mail \n");
+        scanf(" %d, &adressemail");
+
+        printf("Entrez votre mot de paase \n");
+        scanf(" %d, &motdepasse");
+
+        printf("Entrez votre pseudo \n");
+        scanf(" %d, &pseudo");
+
+        printf("Entrez votre prénom \n ");
+        scanf(" %d, &prénom");
+
+        printf("Entrez votre nom \n "),
+        scanf(" %d, &nom");
+
+        printf("Entrez votre age \n");
+        scanf(" %d, &age");
+
+    }
 }
+
+
+
+
+
